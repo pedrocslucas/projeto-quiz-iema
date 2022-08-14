cf78ccbfd8b5e205a118b662db0434d1fbeee55d
Pedro Lucas <79203933+pedrocslucas@users.noreply.github.com>
2022-08-13 21:51:46 -0300

    atualizando o script

--- javascript/script.js
+++ javascript/script.js
@@ -2,21 +2,32 @@ const lista_perguntas = ["Pergunta 1", "Pergunta 2", "Pergunta 3", "Pergunta 4",
 const total = Number(lista_perguntas.length);
 const container = document.getElementById('content');
 
-let index_question = question();
 let resp = document.getElementById('number-question');
+let index_question = get_question();
+
 resp.innerHTML = `<p>${index_question} / ${total}</p>`
 
-function question(){
-    let atual_question = lista_perguntas[0];
-    return lista_perguntas.indexOf(atual_question) + 1;
+function get_question(){
+    let atual_question;
+    let indice;
+    const question_show = document.getElementById('question').textContent;
+    for (let i = 0; i < lista_perguntas.length; i++) {
+        atual_question = lista_perguntas[i];
+        if(atual_question === question_show){
+            indice = lista_perguntas.indexOf(atual_question) + 1;
+            break;
+        }
+    }
+    return indice;
 }
 
 function next(){
     container.style.border = 'none';
-    let id_question = Number(index_question);
-    let show_question = document.getElementById('question');
+    let id_question = get_question();
+    let show_question = document.getElementById('container-question');
     if(id_question != total) {
-        show_question.innerHTML = `<p>${lista_perguntas[id_question]}</p>`
+        resp.innerHTML = `<p>${id_question} / ${total}</p>`
+        show_question.innerHTML = `${lista_perguntas[id_question]}`
     }else {
         show_question.innerHTML = '<p id="congratulations">Fim do Quiz, Obrigado!</p>';
     }
